c1f950c62c4f64b849ee8e94b8cb93acdcb5a1d0
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-09-15 01:55:44 +0800

    .

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,13 @@
+<div
+	class="flex fixed bottom-2 w-screen overflow-x-auto px-2 justify-center select-none scrollbar-none"
+>
+	<div class="flex gap-2">
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/01.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/02.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/03.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/02.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/01.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/01.jpg" alt="" />
+		<img class="w-25 h-15 object-cover cursor-pointer" src="/01.jpg" alt="" />
+	</div>
+</div>
--- src/routes/gs/+page.svelte
+++ src/routes/gs/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import SplatViewer from "$lib/components/SplatViewer.svelte";
+  import Carousel from "$lib/components/Carousel.svelte";
 </script>
 
 <SplatViewer splatURL="/ggun.zip" />
+<Carousel />

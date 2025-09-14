d88e9ee5fd0fba94d354cd8ecd91e266f54957aa
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-09-14 23:22:07 +0800

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,18 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { 
-		adapter: adapter({
-			// Cloudflare Pages configuration
-			routes: {
-				include: ['/*'],
-				exclude: ['<build>']
-			},
-			platformProxy: {
-				persist: false
-			}
-		})
-	}
+	kit: { adapter: adapter() }
 };
 
 export default config;

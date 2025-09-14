81066630dc0275549453c2502aab69595870a2a4
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-09-14 23:27:12 +0800

    .

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// Cloudflare Pages configuration
-			routes: {
-				include: ['/*'],
-				exclude: ['<build>']
-			}
-		})
+		adapter: adapter()
 	}
 };
 

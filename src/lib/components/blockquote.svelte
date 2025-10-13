4b27ff5291a4061cfdb5dfba0a6c102462339976
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-10-14 03:08:35 +0800

    .

---
+++ src/lib/components/blockquote.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<div class="mt-6 mb-2 first:mt-0 italic font-[uoqmunthenkhung]">
+  <i class="fas fa-quote-left"></i>
+  {@render children()}
+</div>
--- src/lib/components/context.svelte
+++ src/lib/components/context.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { fly } from "svelte/transition";
-  const { content } = $props();
+  const { content, title } = $props();
 
   let visible = $state(false);
   onMount(() => {
@@ -10,24 +10,36 @@
 </script>
 
 {#if visible}
-  <div class="fixed bottom-2 left-1/2 -translate-x-1/2">
+  <div
+    class={[
+      "fixed left-1/2 -translate-x-1/2 max-w-[40rem] w-[calc(100vw-1rem)]",
+      window.innerWidth < window.innerHeight ? "bottom-3" : "bottom-12",
+    ]}
+    transition:fly={{ y: 20 }}
+  >
+    <div
+      class="backdrop-blur-xs h-[13rem]"
+      style="position: absolute; bottom: 0; z-index: -1;"
+    ></div>
     <div class="flex">
       <div
         class="rounded-tl-md border-t-3 border-l-3 border-white box-content h-[1em] bg-black/50 w-[2rem]"
       ></div>
-      <div class="bg-black/50 -mt-2.5 px-2 font-bold">唐老鴨</div>
+      <div class="bg-black/50 -mt-2.5 px-2 font-bold">{@render title()}</div>
       <div
         class="rounded-tr-md border-t-3 border-r-3 border-white box-content h-[1em] bg-black/50 flex-grow"
       ></div>
     </div>
     <div
-      class="notice max-w-2xl w-full h-[12rem] bg-black/50 rounded-md border-white border-t-0 rounded-t-none box-content border-3"
+      class="h-[12rem] bg-black/50 rounded-md border-white border-t-0 rounded-t-none box-content border-3"
     >
       <div
         class="pl-4 pr-1 overflow-y-auto h-[11rem] whitespace-pre-line"
-        transition:fly={{ y: 20 }}
+        tabindex="-1"
       >
-        {@render content?.()}
+        <typewritten-text>
+          {@render content()}
+        </typewritten-text>
       </div>
     </div>
   </div>

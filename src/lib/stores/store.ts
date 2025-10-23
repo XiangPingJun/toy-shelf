9b970aeac914be133758f72365225c41bee3a780
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-10-23 11:56:29 +0800

    .

---
+++ src/lib/stores/store.ts
@@ -0,0 +1,8 @@
+import { writable } from 'svelte/store';
+
+export const splatUrl = writable('');
+export const splatPov = writable('');
+export const panUrl = writable('');
+export const panPov = writable('');
+export const heading = writable('');
+export const imgUrl = writable('');

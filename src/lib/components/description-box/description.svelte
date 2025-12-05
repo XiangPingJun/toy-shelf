b5567ead6306a22ff121f4ae0c030766ec9bc613
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-12-05 17:19:17 +0800

    .

--- src/lib/components/description-box/description.svelte
+++ src/lib/components/description-box/description.svelte
@@ -16,10 +16,20 @@
   let intersectionObserver: IntersectionObserver;
 
   onMount(async () => {
+    await new Promise((r) => setTimeout(r, 50));
+    lines[0] = { ...lines[0], text: "" };
     const allLines = [...$activePage.lines];
     while (allLines.length) {
+      while (allLines[0].text.length) {
+        lines[lines.length - 1] = {
+          ...lines[lines.length - 1],
+          text:
+            lines[lines.length - 1].text + allLines[0].text.substring(0, 20),
+        };
+        allLines[0].text = allLines[0].text.substring(20);
+        await new Promise((r) => setTimeout(r, 50));
+      }
       lines.push(allLines.shift());
-      await new Promise((r) => setTimeout(r, 50));
     }
     intersectionObserver = new IntersectionObserver(
       (entries) => {
